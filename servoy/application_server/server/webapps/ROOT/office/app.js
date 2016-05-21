798786b9d45354d88b0c028ebcf25a57e880f3b6
paronne <paronne@servoy.com>
2016-05-21 13:35:33 +0200

    servoy

---
+++ servoy/application_server/server/webapps/ROOT/office/app.js
@@ -0,0 +1,10 @@
+var app = (function(){  // jshint ignore:line
+  'use strict';
+
+  var self = {};
+
+  // Common initialization function (to be called from each page)
+  self.initialize = function(){};
+
+  return self;
+})();
---
+++ servoy/application_server/server/webapps/ROOT/office/office.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict';
+
+  // The initialize function must be run each time a new page is loaded
+  Office.initialize = function(reason){
+    jQuery(document).ready(function(){
+      app.initialize();
+    });
+  };
+  
+})();
